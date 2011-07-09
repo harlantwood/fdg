44d7387aad596fb245048dd538e36c66243adf12
Harlan Knight Wood <git@hkw7.org>
2011-07-08 23:02:51 -0700

    Fresh rails 3.0.9 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fdg::Application.config.secret_token = 'baa80276f34e7561237f23d75d6781cf2b11065a593bcf29d148e98f7ce2bf07096b6c649b72af212c60e8153ff6a9525bbf5ea8b8624587f21d08745c47b19b'
